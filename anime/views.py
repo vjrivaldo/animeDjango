2783e7b840a2e83ff4bbe1e581f2d14db9e50cbc
vjrivaldo <sonodaumi226@gmail.com>
2022-12-24 19:17:40 +0800

    masuk

--- anime/views.py
+++ anime/views.py
@@ -2,6 +2,7 @@ from re import template
 from django.http import HttpResponse,JsonResponse
 from django.shortcuts import render
 import requests
+from series.models import *
 from django.contrib.auth import authenticate, login, logout
 
 def homePage(request):
@@ -21,37 +22,153 @@ def loginPage(request):
     return render(request, template_name)
 
 def popularanime(request):
-    url = 'https://gogoanime.consumet.org/popular'
-    populars = requests.get(url)
-    data_populars = populars.json()
+    data_populars = Animepopular.objects.all()
+    URL = 'https://gogoanime.consumet.org/popular'
+    r = requests.get(URL)
+    data = r.json()
+    for i in data:
+        popular_cek = Animepopular.objects.filter(animeId=i['animeId'])
+        if popular_cek.exists():
+            pop = popular_cek.first()
+            pop.animeTitle = i['animeTitle']
+            pop.animeImg = i['animeImg']
+            pop.releasedDate = i['releasedDate']
+            pop.animeUrl = i['animeUrl']
+        else:
+            Animepopular.objects.create(
+                animeId=i['animeId'],
+                animeTitle=i['animeTitle'],
+                animeImg=i['animeImg'],
+                releasedDate=i['releasedDate'],
+                animeUrl=i['animeUrl']
+            )
+    context = {
+        'data':data,
+        'data_populars':data_populars
+        }
     template_name = "popularanime.html"
-    context = {'data_populars':data_populars}
     return render(request, template_name, context)
+    # url = 'https://gogoanime.consumet.org/popular'
+    # populars = requests.get(url)
+    # data_populars = populars.json()
+    # template_name = "popularanime.html"
+    # context = {'data_populars':data_populars}
+    # return render(request, template_name, context)
 
 def detailanime(request, animeId):
-    url = f'https://gogoanime.consumet.org/anime-details/{animeId}'
-    details = requests.get(url)
-    data_details = details.json()
+    URL = f'https://gogoanime.consumet.org/anime-details/{animeId}'
+    r = requests.get(URL)
+    data = r.json()
+    detail_cek = Detailanime.objects.filter(animeTitle=data['animeTitle'])
+    if detail_cek.exists():
+        det = detail_cek.first()
+        det.animeTitle = data['animeTitle']
+        det.type = data['type']
+        det.releasedDate = data['releasedDate']
+        det.status = data['status']
+        det.otherNames = data['otherNames']
+        det.synopsis = data['synopsis']
+        det.animeImg = data['animeImg']
+        det.totalEpisodes = data['totalEpisodes']
+    else:
+        Detailanime.objects.create(
+            animeId=animeId,
+            animeTitle=data['animeTitle'],
+            type=data['type'],
+            releasedDate=data['releasedDate'],
+            status=data['status'],
+            otherNames=data['otherNames'],
+            synopsis=data['synopsis'],
+            animeImg=data['animeImg'],
+            totalEpisodes=data['totalEpisodes']
+        )
+    data_details = Detailanime.objects.get(animeId=animeId)
+    context = {
+        'data':data,
+        'data_details': data_details
+        }
+    
     template_name = "detailanime.html"
-    context = {'data_details' :data_details}
-    return render (request , template_name, context)
+    return render(request, template_name, context)
+    
+    # url = f'https://gogoanime.consumet.org/anime-details/{animeId}'
+    # details = requests.get(url)
+    # data_details = details.json()
+    # template_name = "detailanime.html"
+    # context = {'data_details' :data_details}
+    # return render (request , template_name, context)
 
 
 def topratedanime(request):
-    url = f'https://gogoanime.consumet.org/top-airing'
-    toprateds = requests.get(url)
-    data_toprateds = toprateds.json()
+    data_toprateds = Topratedanime.objects.all()
+    URL = 'https://gogoanime.consumet.org/top-airing'
+    r = requests.get(URL)
+    data = r.json()
+    for i in data:
+        toprated_cek = Topratedanime.objects.filter(animeId=i['animeId'])
+        if toprated_cek.exists():
+            top = toprated_cek.first()
+            top.animeTitle = i['animeTitle']
+            top.animeImg = i['animeImg']
+            top.latestEpisode = i['latestEp']
+            top.animeUrl = i['animeUrl']
+        else:
+            Topratedanime.objects.create(
+                animeId=i['animeId'],
+                animeTitle=i['animeTitle'],
+                animeImg=i['animeImg'],
+                latestEpisode=i['latestEp'],
+                animeUrl=i['animeUrl']
+            )
+    context = {
+        'data_toprateds1':data,
+        'data_toprateds':data_toprateds
+        
+        }
     template_name = "topratedanime.html"
-    context = {'data_toprateds' :data_toprateds}
-    return render (request , template_name, context)
+    return render(request, template_name, context)
+
+    # url = f'https://gogoanime.consumet.org/top-airing'
+    # toprateds = requests.get(url)
+    # data_toprateds = toprateds.json()
+    # template_name = "topratedanime.html"
+    # context = {'data_toprateds' :data_toprateds}
+    # return render (request , template_name, context)
 
 def movieanime(request):
-    url = 'https://gogoanime.consumet.org/anime-movies'
-    movies = requests.get(url)
-    data_movies = movies.json()
+    data_movies = MovieAnime.objects.all()
+    URL = 'https://gogoanime.consumet.org/anime-movies'
+    r = requests.get(URL)
+    data = r.json()
+    for i in data:
+        movie_cek = MovieAnime.objects.filter(animeId=i['animeId'])
+        if movie_cek.exists():
+            mov = movie_cek.first()
+            mov.animeTitle = i['animeTitle']
+            mov.animeImg = i['animeImg']
+            mov.releasedDate = i['releasedDate']
+            mov.animeUrl = i['animeUrl']
+        else:
+            MovieAnime.objects.create(
+                animeId=i['animeId'],
+                animeTitle=i['animeTitle'],
+                animeImg=i['animeImg'],
+                releasedDate=i['releasedDate'],
+                animeUrl=i['animeUrl']
+            )
+    context = {
+        'data':data,
+        'data_movies':data_movies
+        }
     template_name = "animemovie.html"
-    context = {'data_movies':data_movies}
     return render(request, template_name, context)
+    
+    # url = 'https://gogoanime.consumet.org/anime-movies'
+    # movies = requests.get(url)
+    # data_movies = movies.json()
+    # template_name = "animemovie.html"
+    # context = {'data_movies':data_movies}
+    # return render(request, template_name, context)
 
 def contact(request):
     template_name = "contact.html"
--- series/models.py
+++ series/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Animepopular(models.Model):
+    animeId = models.CharField(max_length=100)
+    animeTitle = models.CharField(max_length=100)
+    animeImg = models.CharField(max_length=100)
+    releasedDate = models.CharField(max_length=100)
+    animeUrl = models.CharField(max_length=100)
+
+
+class Detailanime(models.Model):
+    animeId = models.CharField(max_length=100 ,blank=True, null=True)
+    animeTitle = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    releasedDate = models.CharField(max_length=100)
+    status = models.CharField(max_length=100)
+    otherNames = models.CharField(max_length=100)
+    synopsis = models.CharField(max_length=100)
+    animeImg = models.CharField(max_length=100)
+    totalEpisodes = models.CharField(max_length=100)
+
+class Topratedanime(models.Model):
+    animeId = models.CharField(max_length=100)
+    animeTitle = models.CharField(max_length=100)
+    animeImg = models.CharField(max_length=100)
+    latestEpisode = models.CharField(max_length=100)
+    animeUrl = models.CharField(max_length=100)
+
+
+class MovieAnime(models.Model):
+    animeId = models.CharField(max_length=100)
+    animeTitle = models.CharField(max_length=100)
+    animeImg = models.CharField(max_length=100)
+    releasedDate = models.CharField(max_length=100)
+    animeUrl = models.CharField(max_length=100)
+    
